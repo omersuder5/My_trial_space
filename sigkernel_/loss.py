8e0d86f05891b6ef5964b87b746d6868a7d2c59b
osueder <osueder@ethz.ch>
2026-02-14 12:50:01 +0800

    minor edit in loss

--- sigkernel_/loss.py
+++ sigkernel_/loss.py
@@ -26,26 +26,3 @@ def compute_mmd_loss(kernel, X, output, lead_lag = False, lags = None):
         X = batch_lead_lag_transform(X[:,:,1:], X[:,:,0:1], lags) # inputs are (price series, time dimension, lags to use)
         output = batch_lead_lag_transform(output[:,:,1:], output[:,:,0:1], lags)
     return mmd_loss(X, output, kernel)
-
-
-# def mmd_loss(X: torch.tensor, Y: torch.tensor, kernel: SignatureKernel) -> torch.tensor:
-#     '''
-#     X: torch.tensor of shape (n_samples, n_features)
-#     Y: torch.tensor of shape (n_samples, n_features)
-#     kernel: kernel to be used e.g. SignatureKernel
-#     '''
-
-#     # calculate Gram matrices with normalisation and diagonal of XX/YY zeroed
-#     K_XX = gram(kernel, X,X)
-#     K_YY = gram(kernel, Y,Y)
-#     K_XY = gram(kernel, X,Y)
-
-#     # unbiased MMD statistic (could also use biased, doesn't matter if we use permutation tests)
-#     n = len(K_XX)
-#     m = len(K_YY)
-
-#     mmd = (torch.sum(K_XX[~torch.eye(*K_XX.shape,dtype=torch.bool)]) / (n*(n-1))
-#            + torch.sum(K_YY[~torch.eye(*K_YY.shape, dtype=torch.bool)]) / (m*(m-1))
-#            - 2*torch.sum(K_XY)/(n*m))
-
-#     return mmd
